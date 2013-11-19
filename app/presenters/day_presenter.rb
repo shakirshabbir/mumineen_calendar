1e040f3319b6b04de05664ce71d0784a485ee47e
Murtaza Gulamali <mygulamali@gmail.com>
2013-11-19 12:25:11 +0000

    Add day presenter.

---
+++ app/presenters/day_presenter.rb
@@ -0,0 +1,39 @@
+class DayPresenter
+  def initialize(hijri_date, gregorian_date)
+    @hijri_date = hijri_date
+    @gregorian_date = gregorian_date
+  end
+
+  def hijri_day
+    arabic_numeral(hijri_date.day)
+  end
+
+  def gregorian_day
+    date_format_string = "%-d"
+
+    if hijri_date.day == 1 or gregorian_date.mday == 1
+      date_format_string += " %b"
+    end
+
+    if hijri_date.day == 1 or gregorian_date.yday == 1
+      date_format_string += " %Y"
+    end
+
+    gregorian_date.strftime(date_format_string)
+  end
+
+  private
+
+  attr_reader :hijri_date, :gregorian_date
+
+  def html_codes
+    @html_codes ||= [
+      '&#1632;', '&#1633;', '&#1634;', '&#1635;', '&#1636;',
+      '&#1637;', '&#1638;', '&#1639;', '&#1640;', '&#1641;'
+    ]
+  end
+
+  def arabic_numeral(x)
+    x.to_s.split('').collect { |i| html_codes[i.to_i] } .join
+  end
+end
---
+++ spec/presenters/day_presenter_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe DayPresenter do
+  let(:hijri_date_1) { HijriDate::Date.new(1434, 12, 1) }
+  let(:hijri_date_2) { HijriDate::Date.new(1434, 12, 2) }
+  let(:hijri_date_3) { HijriDate::Date.new(1434, 12, 28) }
+  let(:hijri_date_4) { HijriDate::Date.new(1435, 2, 29) }
+
+  let(:gregorian_date_1) { Date.new(2013, 10, 5) }
+  let(:gregorian_date_2) { Date.new(2013, 10, 6) }
+  let(:gregorian_date_3) { Date.new(2013, 11, 1) }
+  let(:gregorian_date_4) { Date.new(2014, 1, 1) }
+
+  describe "#gregorian_day" do
+    it "presents the Gregorian day, month and year at the beginning of the Hijri month" do
+      presenter = DayPresenter.new(hijri_date_1, gregorian_date_1)
+      presenter.gregorian_day.should == "5 Oct 2013"
+    end
+
+    it "presents just the Gregorian day when it's not the beginning of a Gregorian month or year" do
+      presenter = DayPresenter.new(hijri_date_2, gregorian_date_2)
+      presenter.gregorian_day.should == "6"
+    end
+
+    it "presents the Gregorian day and month at the beginning of a Gregorian month" do
+      presenter = DayPresenter.new(hijri_date_3, gregorian_date_3)
+      presenter.gregorian_day.should == "1 Nov"
+    end
+
+    it "presents the Gregorian day, month and year at the beginning of a Gregorian year" do
+      presenter = DayPresenter.new(hijri_date_4, gregorian_date_4)
+      presenter.gregorian_day.should == "1 Jan 2014"
+    end
+  end
+
+  describe "#arabic_numeral" do
+    it "converts the test numbers to the expected HTML Arabic numerals" do
+      presenter = DayPresenter.new(hijri_date_1, gregorian_date_1)
+      presenter.send(:arabic_numeral, 1).should == "&#1633;"
+      presenter.send(:arabic_numeral, 21).should == "&#1634;&#1633;"
+    end
+  end
+end
