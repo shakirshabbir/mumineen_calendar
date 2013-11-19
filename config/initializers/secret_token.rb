0a100bcd8032e7fbd8bf34d36c241f42bf3804a0
Murtaza Gulamali <mygulamali@gmail.com>
2013-11-19 00:57:17 +0000

    Create new Rails 4.0.1 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MumineenCalendar::Application.config.secret_key_base = '47c5ed0683f86fa7fdec11eae09a2f6b6a2fe281856a652c11af3e9710467c8154bb6b213ebcd817a09d28ddddf356dbe23b68ff41c842341be9cf0e7cb4bafa'
