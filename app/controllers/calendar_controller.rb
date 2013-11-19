df3432fc1d0da4c63e3c040045e8b493a23a5b0c
Murtaza Gulamali <mygulamali@gmail.com>
2013-11-19 15:19:21 +0000

    Get presenter from controller and populate calendar view.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,7 @@
 class CalendarController < ApplicationController
   def index
+    today = HijriDate.jd(Date.today.jd)
+    @year = today.year
+    @calendar = CalendarPresenter.new(@year)
   end
 end
