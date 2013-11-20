ed745b51da2f57802f7a4e70660ea6bf1b58c44a
Murtaza Gulamali <mygulamali@gmail.com>
2013-11-20 17:35:37 +0000

    Style calendar slider previous/next month buttons.

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,15 +1,28 @@
-$(function(){ $(document).foundation(); });
-
 $(document).ready(function () {
-  "use strict";
+  'use strict';
+
+  $(document).foundation();
 
   $('.slider').bxSlider({
     mode: 'horizontal',
     minSlides: 1,
     maxSlides: 1,
-    slideWidth: $('.slider .month').width(),
+    slideWidth: $('.slider table').width(),
     pager: false,
     infiniteLoop: false,
-    hideControlOnEnd: true
+    hideControlOnEnd: true,
+    onSliderLoad: function () {
+      $('#month-meta .title h3').text($('.month').first().data('month'));
+    },
+    onSlidePrev: function (slideElement) {
+      $('#month-meta .title h3').text($(slideElement).data('month'));
+    },
+    onSlideNext: function (slideElement) {
+      $('#month-meta .title h3').text($(slideElement).data('month'));
+    },
+    prevText: '<i class="fi-arrow-left large"></i>',
+    prevSelector: $('#month-meta .prev'),
+    nextText: '<i class="fi-arrow-right large"></i>',
+    nextSelector: $('#month-meta .next')
   });
 });
