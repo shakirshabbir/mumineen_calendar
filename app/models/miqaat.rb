1ab0be10bc55b680d8344b69b10460b48784e87e
Murtaza Gulamali <mygulamali@gmail.com>
2013-12-20 11:40:59 +0000

    Replace miqaats rake task and JSON file with a model.

---
+++ app/models/miqaat.rb
@@ -0,0 +1,42 @@
+require 'csv'
+
+class Miqaat < ActiveFile::Base
+  set_root_path "#{Rails.root}/lib/assets"
+  set_filename "miqaats"
+
+  class << self
+    def extension
+      'csv'
+    end
+
+    def load_file
+      csv_options = {
+        :quote_char => '"',
+        :col_sep => ',',
+        :headers => true,
+        :header_converters => :symbol
+      }
+      year = 1434
+
+      id = 0
+      data = []
+      CSV.foreach(full_path, csv_options) do |row|
+        data << miqaat_hash(id, row)
+        id += 1
+      end
+      data
+    end
+  end
+
+  def self.miqaat_hash(id, data)
+    {
+      id: id,
+      date: data[:date].to_i,
+      month: data[:month].to_i,
+      title: data[:title],
+      description: data[:description],
+      priority: data[:priority].to_i,
+      phase: data[:phase]
+    }
+  end
+end
