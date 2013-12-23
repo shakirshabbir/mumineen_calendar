fb0edc70d775105e285edafbfc23a3ec793e08c3
Murtaza Gulamali <mygulamali@gmail.com>
2013-12-23 10:51:42 +0000

    Add close icon to icon fonts and refactor miqaats modal.

--- app/assets/javascripts/miqaats.js
+++ app/assets/javascripts/miqaats.js
@@ -1,28 +1,12 @@
 var miqaats = (function ($) {
   'use strict';
 
-  var markers = {
-    1: {
-      day: 'icon-sun',
-      night: 'icon-moon'
-    },
-    2: {
-      day: 'icon-sun',
-      night: 'icon-moon'
-    },
-    3: {
-      day: 'icon-circle',
-      night: 'icon-circle'
-    }
-  };
-
   var miqaats = {
     // set it all up
     setup: function () {
       $('.month .day').not('[data-doy=0]').on('click', function (event) {
-        $('#modal').empty();
-        $('#modal').html($(event.target).find('.modal-data').html());
-        $('#modal').append('<a class="close-reveal-modal">&#215;</a>');
+        $('#modal .container').empty();
+        $('#modal .container').html($(event.target).find('.modal-data').html());
         $('#modal').foundation('reveal', 'open');
       });
     }
